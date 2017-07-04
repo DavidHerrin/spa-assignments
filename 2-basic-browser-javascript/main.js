efc93eb20278640e35d1022562fda69a62460b44
David Herrin <david_herrin@hotmail.com>
2017-07-04 16:41:28 -0500

    completed assignment

--- 2-basic-browser-javascript/main.js
+++ 2-basic-browser-javascript/main.js
@@ -1,17 +1,66 @@
 $(document)
+
   .ready(() => {
     'use strict'
-    let autototal = 0
-    let total = 0
-    let amtToSum = 1
+    let autototal = JSON.parse(window.localStorage.getItem('autototal'))
+    if (autototal === null) autototal = 0
+    let total = JSON.parse(window.localStorage.getItem('total'))
+    if (total === null) total = 0
+    let amtToSum = JSON.parse(window.localStorage.getItem('amtToSum'))
+    if (amtToSum === null) amtToSum = 1
+    let intervals = []
+    let intervalcount = JSON.parse(window.localStorage.getItem('intervalcount'))
+    if (intervalcount !== null) {
+      for (let i = 0; i < intervalcount; i++) {
+        intervals.push(
+          setInterval(function () {
+            total += amtToSum
+            $.fn.chgtotal()
+            $.fn.display()
+          }, 1000))
+      }
+    }
+    let resetActive = false
+
+    $(window)
+      .on('unload', function () {
+        $.fn.saveData()
+      })
+
+    $.fn.saveData = function () {
+      window.localStorage.setItem('autototal', JSON.stringify(autototal))
+      window.localStorage.setItem('total', JSON.stringify(total))
+      window.localStorage.setItem('amtToSum', JSON.stringify(amtToSum))
+      window.localStorage.setItem('intervalcount', JSON.stringify(intervals.length))
+    }
+
+    $.fn.reset = function () {
+      for (let i = 0; i < intervals.length; i++) {
+        clearInterval(intervals[i])
+      }
+      intervals.length = 0
+      autototal = 0
+      total = 0
+      amtToSum = 1
+      resetActive = false
+      $.fn.chgtotal()
+      $.fn.display()
+
+    }
 
     $.fn.display = function () {
       $('.total')
         .html(total)
+      $('.adder')
+        .html(amtToSum)
+      $('.autototal')
+        .html(autototal)
+      resetActive = true
     }
 
     $.fn.click1 = function () {
       total += amtToSum
+      resetActive = true
       $.fn.chgtotal()
       $.fn.display()
     }
@@ -20,22 +69,39 @@ $(document)
       if (total >= 10) {
         $('.button2').css({
           "background-color": 'white'
-        });
+        })
       } else {
         $('.button2').css({
           "background-color": 'gray'
-        });
+        })
       }
       if (total >= 100) {
         $('.button3').css({
           "background-color": 'white'
-        });
+        })
       } else {
         $('.button3').css({
           "background-color": 'gray'
-        });
+        })
+      }
+      if (resetActive) {
+        $('.resetbutton').css({
+          "background-color": 'red'
+        })
+      } else {
+        $('.resetbutton').css({
+          "background-color": 'gray'
+        })
       }
     }
+
+    $('.resetbutton')
+      .click(() => {
+        if (resetActive) {
+          $.fn.reset()
+          $.fn.chgtotal()
+        }
+      })
 
     $('.button1')
       .click(() => {
@@ -49,8 +115,6 @@ $(document)
           total -= 10
           $.fn.chgtotal()
           $.fn.display()
-          $('.adder')
-            .html(amtToSum)
         }
       })
 
@@ -61,16 +125,16 @@ $(document)
           total -= 100
           $.fn.chgtotal()
           $.fn.display()
-          $('.autototal')
-            .html(autototal)
-          setInterval(function () {
-            total += amtToSum
-            $.fn.chgtotal()
-            $.fn.display()
-          }, 1000)
+          intervals.push(
+            setInterval(function () {
+              total += amtToSum
+              $.fn.chgtotal()
+              $.fn.display()
+            }, 1000))
         }
       })
 
     $('.total')
       .html(total)
+
   });
